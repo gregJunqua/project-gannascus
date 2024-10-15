95f815932baedd75983eecd81557a8425b80ca2a
moonWalker <greg.junqua@gmail.com>
2024-10-15 23:12:03 +0200

    Feature/initial discovery (#1)
    * feat: first commit
    - model still under investigation not included
    
    * feat: cropping picture
    
    * fix:
    - Now we are using library
    - Changing file organization

---
+++ src/gannascus/gannascus.core/PictureAnalysis/PictureAnalyser.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+using Tesseract;
+
+namespace gannascus.core.PictureAnalysis;
+
+public record Localisation(string Zone);
+
+public class PictureAnalyser
+{
+    public Localisation Analyse(string path)
+    {
+        using var fullImage = new Bitmap(path);
+        var cropRect = new Rectangle(fullImage.Width/2,0,fullImage.Width/2,fullImage.Height/2);
+        using var croppedImage = fullImage.Clone(cropRect, fullImage.PixelFormat);
+        using var img = PixConverter.ToPix(croppedImage);
+        using var ocrEngine = new TesseractEngine(@"./PictureAnalysis/tessdata", "eng", EngineMode.Default);
+        using var page = ocrEngine.Process(img);
+        return new Localisation(page.GetText().Trim());
+    }
+}
---
+++ tests/gannascus.picturehandlingTests/UnitTest1.cs
@@ -0,0 +1,30 @@
+using FluentAssertions;
+using gannascus.core.PictureAnalysis;
+
+namespace gannascus.picturehandlingTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void reading_area_validation_dark_background()
+    {
+        var path = Directory.GetCurrentDirectory();
+        var pictureAnalyser = new PictureAnalyser();
+        var localisation = pictureAnalyser.Analyse(path+"/assets/testPicture1.jpg");
+        localisation.Should().Be("OOC_Stanton_3_Arcorp");
+    }
+    
+    [Test]
+    public void reading_area_validation_lighted_background()
+    {
+        var path = Directory.GetCurrentDirectory(); 
+        var pictureAnalyser = new PictureAnalyser();
+        var localisation = pictureAnalyser.Analyse(path+"assets/testPicture2.jpg");
+        localisation.Should().Be("OOC_Stanton_3_Arcorp");
+    }
+}
