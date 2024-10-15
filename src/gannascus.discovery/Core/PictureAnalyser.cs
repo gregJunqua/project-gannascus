1e12220051a3e1bb60d0740b6724ec4d7d8a0376
gregoire.junqua <greg.junqua@gmail.com>
2024-10-15 21:21:12 +0200

    feat: cropping picture

--- src/gannascus.discovery/Core/PictureAnalyser.cs
+++ src/gannascus.discovery/Core/PictureAnalyser.cs
@@ -9,8 +9,11 @@ public class PictureAnalyser
 {
     public Localisation Analyse(string path)
     {
+        using var fullImage = new Bitmap(path);
+        var cropRect = new Rectangle(fullImage.Width/2,0,fullImage.Width/2,fullImage.Height/2);
+        using var croppedImage = fullImage.Clone(cropRect, fullImage.PixelFormat);
+        using var img = PixConverter.ToPix(croppedImage);
         using var ocrEngine = new TesseractEngine(@"./Core/tessdata", "eng", EngineMode.Default);
-        using var img = Pix.LoadFromFile(path);
         using var page = ocrEngine.Process(img);
         return new Localisation(page.GetText().Trim());
     }
